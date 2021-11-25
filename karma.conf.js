e2fa5b035e0060c1fc147971b92e095e21293809
Konrad Linkowski <linkowski.konrad97@gmail.com>
2021-11-25 13:33:23 +0100

    Rename project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/word-cloud-game'),
+      dir: require('path').join(__dirname, './coverage/wordcloud-game'),
       subdir: '.',
       reporters: [
         { type: 'html' },
